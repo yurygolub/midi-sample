d6d3e3bee0f5fd4a2ccf93ca9805a86d86ee6fc3
Yury Golub <yuragolub21@gmail.com>
2023-08-04 21:01:17 +0300

    Implement export to midi file and playback from midi file.

--- MidiSample/Program.cs
+++ MidiSample/Program.cs
@@ -1,20 +1,36 @@
 ﻿using System;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
+using System.Threading;
 using NAudio.Midi;
 
 namespace MidiSample
 {
     internal class Program
     {
+        private const int MidiInDeviceIndex = 0;
+        private const int MidiOutDeviceIndex = 1;
+
+        private const string OutPath = "out.mid";
+        private const string MidiFilePath = @"Linkin Park - Numb (Tim Dawes Remix).mid";
+
+        private const int MidiFileType = 0;
+        private const int DeltaTicksPerQuarterNote = 120;
+        private static readonly MidiEventCollection MidiEvents = new (MidiFileType, DeltaTicksPerQuarterNote);
+
+        private static readonly Stopwatch Stopwatch = new ();
+        private static long absoluteTime;
+
         private static void Main()
         {
-            MIDIEvents();
-            MIDIFiles();
+            Events();
+            Files();
         }
 
-        private static void MIDIEvents()
+        private static void Events()
         {
-            Console.WriteLine("MIDIEvents example:");
+            Console.WriteLine("Events example:");
             Console.WriteLine("MidiIn:");
             for (int device = 0; device < MidiIn.NumberOfDevices; device++)
             {
@@ -31,26 +47,31 @@ namespace MidiSample
 
             Console.WriteLine();
 
-            int deviceIndex = 0;
-            var midiIn = new MidiIn(deviceIndex);
+            var midiIn = new MidiIn(MidiInDeviceIndex);
             midiIn.MessageReceived += MidiIn_MessageReceived;
             midiIn.ErrorReceived += MidiIn_ErrorReceived;
             midiIn.Start();
 
-            int midiOutDeviceIndex = 1;
-            var midiOut = new MidiOut(midiOutDeviceIndex);
+            var midiOut = new MidiOut(MidiOutDeviceIndex);
             int channel = 1;
             int noteNumber = 50;
             var noteOnEvent = new NoteOnEvent(0, channel, noteNumber, 100, 50);
             midiOut.Send(noteOnEvent.GetAsShortMessage());
             midiOut.Dispose();
 
+            Console.WriteLine("Press escape to exit");
             while (Console.ReadKey(true).Key != ConsoleKey.Escape)
             {
             }
 
             midiIn.Stop();
             midiIn.Dispose();
+
+            MidiEvents.PrepareForExport();
+
+            MidiFile.Export(OutPath, MidiEvents);
+            Console.WriteLine($"Notes exported to file: {Path.GetFullPath(OutPath)}");
+            Console.WriteLine();
         }
 
         private static void MidiIn_ErrorReceived(object sender, MidiInMessageEventArgs e)
@@ -64,33 +85,55 @@ namespace MidiSample
                 && e.MidiEvent.CommandCode != MidiCommandCode.AutoSensing)
             {
                 Console.WriteLine($"Message: Time {e.Timestamp} Message 0x{e.RawMessage:X8} Event {e.MidiEvent}");
-            }
-        }
 
-        private static void MIDIFiles()
-        {
-            Console.WriteLine("MIDIFiles example:");
-            var strictMode = false;
-            string path = @"Linkin Park - Numb (Tim Dawes Remix).mid";
-            var mf = new MidiFile(path, strictMode);
-
-            Console.WriteLine($"Format {mf.FileFormat}, Tracks {mf.Tracks}, Delta Ticks Per Quarter Note {mf.DeltaTicksPerQuarterNote}");
-
-            var timeSignature = mf.Events[0].OfType<TimeSignatureEvent>().FirstOrDefault();
-
-            for (int n = 0; n < mf.Tracks; n++)
-            {
-                foreach (var midiEvent in mf.Events[n])
+                if (e.MidiEvent is NoteOnEvent noteOnEvent)
                 {
-                    if (!MidiEvent.IsNoteOff(midiEvent))
-                    {
-                        Console.WriteLine($"{ToMBT(midiEvent.AbsoluteTime, mf.DeltaTicksPerQuarterNote, timeSignature)} {midiEvent}");
-                    }
+                    Stopwatch.Stop();
+                    absoluteTime += Stopwatch.ElapsedMilliseconds;
+                    noteOnEvent.AbsoluteTime = absoluteTime;
+                    noteOnEvent.NoteLength = 100;
+                    MidiEvents.AddEvent(noteOnEvent, 0);
+                    Stopwatch.Restart();
                 }
             }
         }
 
-        private static string ToMBT(long eventTime, int ticksPerQuarterNote, TimeSignatureEvent timeSignature)
+        private static void Files()
+        {
+            Console.WriteLine("Files example:");
+            bool strictMode = false;
+            var midiFile = new MidiFile(MidiFilePath, strictMode);
+
+            Console.WriteLine($"Format {midiFile.FileFormat}, Tracks {midiFile.Tracks}, Delta Ticks Per Quarter Note {midiFile.DeltaTicksPerQuarterNote}");
+
+            TimeSignatureEvent timeSignature = midiFile.Events[0].OfType<TimeSignatureEvent>().FirstOrDefault();
+
+            var midiOut = new MidiOut(MidiOutDeviceIndex);
+            int absoluteTime = 0;
+
+            for (int i = 0; i < midiFile.Tracks; i++)
+            {
+                foreach (MidiEvent midiEvent in midiFile.Events[i])
+                {
+                    if (!MidiEvent.IsNoteOff(midiEvent))
+                    {
+                        Console.WriteLine($"{ToMeasuresBeatsTicks(midiEvent.AbsoluteTime, midiFile.DeltaTicksPerQuarterNote, timeSignature)} {midiEvent}");
+                    }
+
+                    if (midiEvent.AbsoluteTime > absoluteTime)
+                    {
+                        Thread.Sleep((int)midiEvent.AbsoluteTime - absoluteTime);
+                        absoluteTime = (int)midiEvent.AbsoluteTime;
+                    }
+
+                    midiOut.Send(midiEvent.GetAsShortMessage());
+                }
+            }
+
+            midiOut.Dispose();
+        }
+
+        private static string ToMeasuresBeatsTicks(long eventTime, int ticksPerQuarterNote, TimeSignatureEvent timeSignature)
         {
             int beatsPerBar = timeSignature == null
                 ? 4
@@ -98,7 +141,7 @@ namespace MidiSample
 
             int ticksPerBar = timeSignature == null
                 ? ticksPerQuarterNote * 4
-                : (timeSignature.Numerator * ticksPerQuarterNote * 4) / (1 << timeSignature.Denominator);
+                : timeSignature.Numerator * ticksPerQuarterNote * 4 / (1 << timeSignature.Denominator);
 
             int ticksPerBeat = ticksPerBar / beatsPerBar;
             long bar = 1 + (eventTime / ticksPerBar);
